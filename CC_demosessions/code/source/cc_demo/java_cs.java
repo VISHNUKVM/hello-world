1e91eab84a7588771dfc5c158dad9fc5f7c4915d
vishnu <vishnu.kvm@gmail.com>
2017-07-21 17:28:48 +0530

    training pkg from vishnu-v1.0

---
+++ CC_demosessions/code/source/cc_demo/java_cs.java
@@ -0,0 +1,55 @@
+package cc_demo;
+
+// -----( IS Java Code Template v1.2
+
+import com.wm.data.*;
+import com.wm.util.Values;
+import com.wm.app.b2b.server.Service;
+import com.wm.app.b2b.server.ServiceException;
+// --- <<IS-START-IMPORTS>> ---
+// --- <<IS-END-IMPORTS>> ---
+
+public final class java_cs
+
+{
+	// ---( internal utility methods )---
+
+	final static java_cs _instance = new java_cs();
+
+	static java_cs _newInstance() { return new java_cs(); }
+
+	static java_cs _cast(Object o) { return (java_cs)o; }
+
+	// ---( server methods )---
+
+
+
+
+	public static final void program1 (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(program1)>> ---
+		// @sigtype java 3.5
+		// [i] field:0:required strA
+		// [i] field:0:required strB
+		// [o] field:0:required result
+		// pipeline
+		IDataCursor pipelineCursor = pipeline.getCursor();
+			String	strA = IDataUtil.getString( pipelineCursor, "strA" );
+			String	strB = IDataUtil.getString( pipelineCursor, "strB" );
+		pipelineCursor.destroy();
+		
+		String result;
+		result = strA + strB;
+		// pipeline
+		IDataCursor pipelineCursor_1 = pipeline.getCursor();
+		IDataUtil.put( pipelineCursor_1, "result", result );
+		pipelineCursor_1.destroy();
+		
+			
+		// --- <<IS-END>> ---
+
+                
+	}
+}
+
---
+++ CC_demosessions/ns/cc_demo/java_cs/program1/java.frag
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+
+<Values version="2.0">
+  <value name="name">program1</value>
+  <value name="encodeutf8">true</value>
+  <value name="body">Ly8gcGlwZWxpbmUNCklEYXRhQ3Vyc29yIHBpcGVsaW5lQ3Vyc29yID0gcGlwZWxpbmUuZ2V0Q3Vy
+c29yKCk7DQoJU3RyaW5nCXN0ckEgPSBJRGF0YVV0aWwuZ2V0U3RyaW5nKCBwaXBlbGluZUN1cnNv
+ciwgInN0ckEiICk7DQoJU3RyaW5nCXN0ckIgPSBJRGF0YVV0aWwuZ2V0U3RyaW5nKCBwaXBlbGlu
+ZUN1cnNvciwgInN0ckIiICk7DQpwaXBlbGluZUN1cnNvci5kZXN0cm95KCk7DQoNClN0cmluZyBy
+ZXN1bHQ7DQpyZXN1bHQgPSBzdHJBICsgc3RyQjsNCi8vIHBpcGVsaW5lDQpJRGF0YUN1cnNvciBw
+aXBlbGluZUN1cnNvcl8xID0gcGlwZWxpbmUuZ2V0Q3Vyc29yKCk7DQpJRGF0YVV0aWwucHV0KCBw
+aXBlbGluZUN1cnNvcl8xLCAicmVzdWx0IiwgcmVzdWx0ICk7DQpwaXBlbGluZUN1cnNvcl8xLmRl
+c3Ryb3koKTsNCg0KCQ==</value>
+</Values>
